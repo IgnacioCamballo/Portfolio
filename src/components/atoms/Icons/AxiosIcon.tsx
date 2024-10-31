cb1c9b060a307034c35a5ed4ca83720aad288638
Ignacio Camballo <ignaciocamballo@gmail.com>
2024-10-31 00:06:05 +0000

    sets color to white

--- src/components/atoms/Icons/AxiosIcon.tsx
+++ src/components/atoms/Icons/AxiosIcon.tsx
@@ -5,7 +5,7 @@ export default function AxiosIcon({ width, height }: Icon) {
   return (
     <div className={styles.techCont}>
       <div style={{height: height, width: width, display: "flex", alignItems: "center"}}>
-        <img src="./dist/assets/axios.png" width={width} height={height/3}/>
+        <img src="/assets/axios.png" width={width} height={height/3}/>
       </div>
 
       <p className={styles.techName}>Axios</p>
